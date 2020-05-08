c65ec44c7436d5b15398db599872bd531564fc68
Brunin <bruno.matheaco@gmail.com>
2020-05-08 16:09:05 -0300

    Criamos a tela de lool

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<?php 
-
+<?php
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$sql = new Hcode\DB\Sql();
     
 	$page = new Page();
 
@@ -19,6 +22,8 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
@@ -26,6 +31,35 @@ $app->get('/admin', function() {
 
 });
 
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
 $app->run();
 
- ?>
+?>
