8041c713368dc818a1b10db1cfdee51d10d71b63
Brunin <bruno.matheaco@gmail.com>
2020-06-01 17:58:54 -0300

    Paginação

--- view-cache/categories-menu.5374d2fe8e9cb7f39df798149dc2ecc3.rtpl.php
+++ view-cache/categories-menu.5374d2fe8e9cb7f39df798149dc2ecc3.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/6">Motorlola</a></li><li><a href="/categories/7">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/15">iOS</a></li>
